a42273be0cfd9059570c429dfb684d3c9bb20828
sylverpyro <sylverpyro@users.noreply.github.com>
2025-02-06 21:22:57 -0600

    Update unzip-dir-content.ps1

--- unzip-dir-content.ps1
+++ unzip-dir-content.ps1
@@ -1,7 +1,11 @@
 Get-ChildItem -Filter *.zip | ForEach-Object { 
   $basename=[System.Io.Path]::GetFileNameWithoutExtension($_)
   Write-Output $basename
-  New-Item -Path .\unqipped -ItemType Directory
-  New-Item -Path .\unqipped\$basename -ItemType Directory
-  Expand-Archive -Path $_.FullName -DestinationPath unzipped/$basename  
+  $outdir=".\unzipped"
+  $dest="$outdir\$basename"
+  if (-not (Test-Path -Path $dest)) {
+    New-Item -Path $dest -ItemType Directory
+  }
+  Expand-Archive -Force -Path $_.FullName -DestinationPath unzipped/$basename
+  break
 }
